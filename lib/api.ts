c90d27fc0dc2b49963c5373d1ec6f9a9b796d529
yunyoungsik <yunyoungsik91@gmail.com>
2024-01-17 11:45:47 +0900

    수정

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import { join } from "path";
 import matter from "gray-matter";
 
+const config = require('../next.config');
 const postsDirectory = join(process.cwd(), "_posts");
 
 export function getPostSlugs() {
@@ -18,7 +19,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
     [key: string]: string;
   };
 
-  const items: Items = {};
+  let items: Items = {};
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
@@ -33,6 +34,11 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
       items[field] = data[field];
     }
   });
+
+  // Replace image
+  let itemStr = JSON.stringify(items);
+  itemStr = itemStr.replaceAll(/\${basePath}/gi, config.basePath);
+  items = JSON.parse(itemStr)
 
   return items;
 }
